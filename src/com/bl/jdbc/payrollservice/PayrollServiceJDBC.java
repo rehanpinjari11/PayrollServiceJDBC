90e387217f590eccbbb125a5bc75b529fef9b827
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-16 11:10:09 +0530

    Added: Welcome Message is added

---
+++ src/com/bl/jdbc/payrollservice/PayrollServiceJDBC.java
@@ -0,0 +1,11 @@
+package com.bl.jdbc.payrollservice;
+
+public class PayrollServiceJDBC {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to JDBC Connection");
+
+    }
+
+}
